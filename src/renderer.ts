f181209a6b6186f13aa602bae702772ce62d251e
Daniel Limberger <cgdaniel@outlook.com>
2022-02-08 15:13:14 +0100

    quicksave

--- src/renderer.ts
+++ src/renderer.ts
@@ -23,6 +23,7 @@ import { GLTFPass } from "./gltf-pass";
 import { GeometryVertexShader, GeometryFragmentShader } from "./shaders";
 
 export class TestRenderer extends Renderer {
+  protected _startTime: number;
   protected _defaultFBO: DefaultFramebuffer;
 
   protected _navigation: Navigation;
@@ -30,16 +31,22 @@ export class TestRenderer extends Renderer {
 
   protected _lightsPass: LightsPass;
   protected _geometryPass: GeometryPass;
+  protected _lightsBuffer: WebGLBuffer;
 
   protected _program: Program;
   protected _uViewProjection: WebGLUniformLocation;
   protected _uModel: WebGLUniformLocation;
+  protected _uTime: WebGLUniformLocation;
+  protected _uEye: WebGLUniformLocation;
+  protected _uLights: WebGLUniformLocation;
 
   protected onInitialize(
     context: Context,
     callback: Invalidate,
     eventProvider: EventProvider
   ): boolean {
+    this._startTime = Date.now();
+
     this._defaultFBO = new DefaultFramebuffer(context, "DefaultFBO");
     this._defaultFBO.initialize();
 
@@ -70,6 +77,29 @@ export class TestRenderer extends Renderer {
 
     const gl = context.gl;
 
+    this._lightsBuffer = gl.createBuffer();
+    const lights = new Float32Array([
+      // x, y, z, r, g, b, data,
+      0.5,
+      1.3,
+      1.5,
+      1.0,
+      0.9,
+      0.1,
+      40.0,
+      -2.2,
+      1.3,
+      0.4,
+      0.0,
+      0.8,
+      1.0,
+      40.0
+    ]);
+
+    gl.bindBuffer(gl.UNIFORM_BUFFER, this._lightsBuffer);
+    gl.bufferData(gl.UNIFORM_BUFFER, lights, gl.DYNAMIC_DRAW);
+    gl.bindBuffer(gl.UNIFORM_BUFFER, null);
+
     const vert = new Shader(context, gl.VERTEX_SHADER, "geometry.vert");
     vert.initialize(GeometryVertexShader);
     const frag = new Shader(context, gl.FRAGMENT_SHADER, "geometry.frag");
@@ -85,6 +115,9 @@ export class TestRenderer extends Renderer {
 
     this._uViewProjection = this._program.uniform("u_viewProjection");
     this._uModel = this._program.uniform("u_model");
+    this._uTime = this._program.uniform("u_time");
+    this._uEye = this._program.uniform("u_eye");
+    this._uLights = this._program.uniform("u_lights");
     const identity = mat4.identity(mat4.create());
     gl.uniformMatrix4fv(this._uModel, false, identity);
 
@@ -110,7 +143,7 @@ export class TestRenderer extends Renderer {
     this._lightsPass.update();
     this._geometryPass.update();
 
-    return this._altered.any || this._camera.altered;
+    return this._altered.any || this._camera.altered || true;
   }
 
   protected onPrepare(): void {
@@ -141,6 +174,21 @@ export class TestRenderer extends Renderer {
       false,
       this._camera.viewProjection
     );
+    const timePassed = (Date.now() - this._startTime) / 1000.0;
+    gl.uniform1f(this._uTime, timePassed);
+    gl.uniform3fv(this._uEye, this._camera.eye);
+    /*
+    const lightsBlockIndex = gl.getUniformBlockIndex(
+      this._program.object,
+      "ub_lights"
+    );
+    gl.uniformBlockBinding(
+      this._program.object,
+      lightsBlockIndex,
+      this._uLights
+    );
+    gl.bindBufferBase(gl.UNIFORM_BUFFER, lightsBlockIndex, this._lightsBuffer);
+    */
 
     gl.enable(gl.CULL_FACE);
     gl.cullFace(gl.BACK);
@@ -152,6 +200,8 @@ export class TestRenderer extends Renderer {
 
     gl.cullFace(gl.BACK);
     gl.disable(gl.CULL_FACE);
+
+    this.invalidate(true);
   }
 
   protected onSwap(): void {}
--- src/shaders.ts
+++ src/shaders.ts
@@ -40,43 +40,69 @@ const float light1Intensity = 1.0;
 
 const vec3 ambient = vec3(0.05, 0.06, 0.07);
 
+uniform vec3 u_eye;
+uniform float u_time;
+
+vec3 computeNormalFromPosition(in vec3 position) {
+
+  vec3 N = position;
+  // if(abs(N.y) < abs(N.z) && abs(N.x) < abs(N.z)) {
+  //   N = vec3(0.0, 0.0, sign(N.z));
+  // }
+  // else if(abs(N.x) < abs(N.y) && abs(N.z) < abs(N.y)) {
+  //   N = vec3(0.0, sign(N.y), 0.0);
+  // }
+  // else if(abs(N.y) < abs(N.x) && abs(N.z) < abs(N.x)) {
+  //   N = vec3(sign(N.x), 0.0, 0.0);
+  // }
+
+  /* NVIDIA QUADRO WORKAROUNDS (WS: Pleasers) */
+  return (floor(abs(N) + vec3(2.0)) - vec3(2.0)) * sign(N);
+  // return (round(abs(N) - vec3(0.4999))) * sign(N);
+  
+  return floor(abs(N)) * sign(N);
+}
+
+const float PI = 3.1415926535897932384626433832795;
+const float DEG2RAD = PI / 180.0;
+
+vec3 phong(in vec3 lightPosition, in vec4 lightColor, in vec3 position, in vec3 normal, in vec3 eye) {
+
+  float attenuation = 10.0 * pow(length(position - lightPosition), -2.0);
+  float intensity = 0.0;
+  float specular = 0.0;
+
+  vec3 N = normalize(normal); 
+  vec3 L = normalize(lightPosition - position); // surface position to light
+  vec3 E = normalize(eye - position); // surface position to camera eye
+  vec3 R = reflect(-L, N);
+  vec3 H = normalize(L + E);
+
+  // vec3 lightDirection = normalize(vec3(0.0, 0.0, -1.0));
+  // float cutoff = 0.6;//10.0 * DEG2RAD;
+  
+  // if(dot(L, -lightDirection) >= cutoff) {
+
+    float NdotL = max(dot(N, L), 0.0); // lambert
+    float EdotR = max(dot(R, E), 0.0);
+    float NdotH = max(dot(N, H), 0.0);
+
+    intensity = NdotL; // * attenuation;
+    // specular = clamp(pow(EdotR, 16.0), 0.0, 1.0);
+    specular = clamp(pow(NdotH, 16.0), 0.0, 1.0);
+  // }
+
+  return  vec3(intensity + specular); // * lightColor.rgb;
+}
+
 void main(void)
 {
-  // fragColor = vec4(v_vertex.xyz * 0.5 + 0.5, 1.0);
-  vec3 normal = v_vertex.xyz;
-  // vec3 normal = normalize(v_normal);
+  vec3 N = computeNormalFromPosition(v_vertex.xyz);
 
-  vec3 N = normal;
-  // if(abs(n.y) < abs(n.z) && abs(n.x) < abs(n.z)) {
-  //   normal = vec3(0.0, 0.0, sign(n.z));
-  // }
-  // else if(abs(n.x) < abs(n.y) && abs(n.z) < abs(n.y)) {
-  //   normal = vec3(0.0, sign(n.y), 0.0);
-  // }
-  // else if(abs(n.y) < abs(n.x) && abs(n.z) < abs(n.x)) {
-  //   normal = vec3(sign(n.x), 0.0, 0.0);
-  // }
-  N = floor(abs(N)) * sign(N); // same as above ;)
+  vec3 color = vec3(0.0);
+  color += phong(light0Position, vec4(light0Color, light0Intensity), v_vertex.xyz, N, u_eye);
+  // color += phong(light1Position, vec4(light1Color, light1Intensity), v_vertex.xyz, N, u_eye);
 
-
-  vec3 L0 = normalize(light0Position - v_vertex.xyz);
-  float i0 = clamp(dot(L0, N), 0.0, 1.0); // [-1,+1]
-  vec3 L1 = normalize(light1Position - v_vertex.xyz);
-  float i1 = clamp(dot(L1, N), 0.0, 1.0); // [-1,+1]
-
-  // float attenuation = pow(length(v_vertex.xyz - light0Position), -2.0);
-  float attenuation0 = 1.0 / length(v_vertex.xyz - light0Position);
-  float attenuation1 = 1.0 / length(v_vertex.xyz - light1Position);
-  attenuation0 *= light0Intensity;
-  attenuation1 *= light1Intensity;
-
-  // fragColor = vec4(N * 0.5 + 0.5, 1.0);
-  // fragColor = vec4(vec3(i), 1.0);
-  // fragColor = vec4(vec3(attenuation * i), 1.0);
-  
-  fragColor = vec4(
-    mix(ambient, light0Color, attenuation0 * i0) +
-    mix(ambient, light1Color, attenuation1 * i1)
-    , 1.0);
+  fragColor = vec4(color, 1.0);
 }
 `;
